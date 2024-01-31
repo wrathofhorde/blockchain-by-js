42326b2b53c414f08602d7f60def2cedc5590966
ryan <wrathofhorde@gmail.com>
2024-02-01 00:26:18 +0900

    add skeleton files

---
+++ dev/blockchain.js
@@ -0,0 +1,9 @@
+const Blockchain =>() {
+    this.chain = [];
+    this.pendingTransaction = [];
+    
+    this.networkNodes = [];
+    this.currentNodelUrl = currentNodelUrl;
+
+    this.createNewBlock(100, '0', '0');
+}
